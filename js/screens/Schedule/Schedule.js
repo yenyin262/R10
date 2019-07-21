34994fc078b9a92ef2554d971f4d50ea53c02d37
Yen Yin Hing <yenyinf287@gmail.com>
2019-07-21 15:44:10 -0700

    attempt to implement helper fn

--- js/screens/Schedule/Schedule.js
+++ js/screens/Schedule/Schedule.js
@@ -1,7 +1,8 @@
 // expect data and display it
 import React from "react";
 import { View, Text, ScrollView } from "react-native";
-// import {formatSessionData} from "../../lib/dataFormatForSchedule"
+import { FlatList } from "react-native-gesture-handler";
+import { formatSessionData } from "../../lib/dataFormatForSchedule";
 
 const Schedule = ({ data }) => {
   return (
@@ -25,4 +26,44 @@ const Schedule = ({ data }) => {
   );
 };
 
+// <Scroll>
+
+{
+  /* <SectionList
+  // need to get data
+  section={data}
+  data={formatSessionData(data)}
+  // render each item in each data of array
+  renderItem={({ session }) => (
+    <View>
+      <View>
+        <Text>{session.startTime}</Text>
+      </View>
+      <View>
+        <Text>{session.title}</Text>
+      </View>
+      <View>
+        <Text>{session.title}</Text>
+      </View>
+    </View>
+  )}
+  KeyExtractor={session => session.id}
+/>; */
+}
+
 export default Schedule;
+// {data.allSessions.map(({ id, startTime, title, location }) => {
+//   return (
+//     <View key={id}>
+//       <View>
+//         <Text>{startTime}</Text>
+//       </View>
+//       <View>
+//         <Text>{title}</Text>
+//       </View>
+//       <View>
+//         <Text>{location}</Text>
+//       </View>
+//     </View>
+//   );
+// })}
