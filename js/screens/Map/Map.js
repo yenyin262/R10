4ea163310cc433a01015f58b474079eccd98deea
Yen Yin Hing <yenyinf287@gmail.com>
2019-07-21 00:12:16 -0700

    implement Map Tab

---
+++ js/screens/Map/Map.js
@@ -0,0 +1,24 @@
+// expect data and display it
+import React from "react";
+import { View, Text, ScrollView } from "react-native";
+
+const Map = ({ data }) => {
+  return (
+    <ScrollView>
+      {data.allConducts.map(({ title, description }) => {
+        return (
+          <View key={title}>
+            <View>
+              <Text>{title}</Text>
+            </View>
+            <View>
+              <Text>{description}</Text>
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+export default Map;
---
+++ js/screens/Map/index.js
@@ -0,0 +1,4 @@
+import MapContainer from "./MapContainer";
+import Map from "./Map";
+export { Map }; // destructure if i want to get about without the about container component
+export default MapContainer;
