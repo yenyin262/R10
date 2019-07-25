f11d83376723567ffb6ab78f2e23152ae4648b6e
Yen Yin Hing <yenyinf287@gmail.com>
2019-07-25 15:42:58 -0700

    implement hamburger menu

--- js/navigation/config.js
+++ js/navigation/config.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Platform } from "react-native";
 import { Header } from "react-navigation";
 import LinearGradient from "react-native-linear-gradient";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const GradientHeader = props => (
   <View style={{ backgroundColor: "white", overflow: "hidden" }}>
@@ -15,9 +16,21 @@ const GradientHeader = props => (
   </View>
 );
 
+const AndroidMenu = ({ navigation }) => (
+  <TouchableOpacity
+    style={{ marginHorizontal: 20 }}
+    onPress={() => navigation.openDrawer()}
+  >
+    <Icon name="md-menu" size={25} color="white" />
+  </TouchableOpacity>
+);
 export const sharedNavigationOptions = navigation => ({
   headerBackTitle: null,
   header: props => <GradientHeader {...props} />,
+  headerLeft: Platform.select({
+    android: <AndroidMenu navigation={navigation} />
+  }),
+
   headerStyle: {
     backgroundColor: "transparent",
     fontFamily: "Montserrat",
