bd9c6d87232e8d2d68764014e0012e7e9c482acf
Yen Yin Hing <yenyinf287@gmail.com>
2019-07-26 01:29:54 -0700

    btn functionality

--- js/screens/Sessions/Sessions.js
+++ js/screens/Sessions/Sessions.js
@@ -12,12 +12,89 @@ import {
 import moment from "moment";
 import LinearGradient from "react-native-linear-gradient";
 
-const Sessions = ({ session, speaker, navigation, favesFunctionality }) => {
-  console.log(favesFunctionality, "this is a speaker");
+const Sessions = ({
+  session,
+  speaker,
+  navigation,
+  addFaves,
+  removesFaves,
+  checkForFaves
+}) => {
+  let favesButton;
+
+  // let myFaves = await checkForFaves(session.id)
+  // if number is in allowed, render add button
+  // otherwise, render delete button
+  console.log(checkForFaves, "checking ");
+
+  // if (allowed.includes(randomNumber)) {
+
+  //put checkforFaves in component did mmount change ot class based component
+  // .then - have data this.setstate and update this.state.isAdd
+  //need to put boolean state if is add - true false = remove
+  // this.state.isAdd
+  // in render conditionally render or based on what this.state.isAdd is
+
+  checkForFaves(session.id)
+    .then(listOfFaves => {
+      console.log(listOfFaves);
+      if (listOfFaves.includes(session.id)) {
+        favesButton = (
+          <View style={styles.favebuttonContainer}>
+            <LinearGradient
+              colors={["#cf392a", "#9963ea"]}
+              start={{ x: 0.0, y: 1.0 }}
+              end={{ x: 1.0, y: 0.0 }}
+              style={[
+                StyleSheet.absoluteFill,
+                { width: "65%" },
+                styles.favebutton
+              ]}
+            >
+              <TouchableOpacity
+                onPress={() => {
+                  removesFaves(session.id);
+                }}
+              >
+                <Text style={styles.faveText}> Remove from Faves </Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        );
+      } else {
+        favesButton = (
+          <View style={styles.favebuttonContainer}>
+            <LinearGradient
+              colors={["#cf392a", "#9963ea"]}
+              start={{ x: 0.0, y: 1.0 }}
+              end={{ x: 1.0, y: 0.0 }}
+              style={[
+                StyleSheet.absoluteFill,
+                { width: "65%" },
+                styles.favebutton
+              ]}
+            >
+              <TouchableOpacity
+                onPress={() => {
+                  addFaves(session.id);
+                }}
+              >
+                <Text style={styles.faveText}> Add to Faves </Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        );
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
   return (
     <ScrollView>
       <View>
         <Text style={styles.locationTitle}>{session.location}</Text>
+
         <Text style={styles.sessionTitle}>{session.title}</Text>
         <Text style={styles.time}>
           {moment(session.startTime).format("LT")}
@@ -38,8 +115,8 @@ const Sessions = ({ session, speaker, navigation, favesFunctionality }) => {
         </TouchableOpacity>
 
         <View style={styles.lineSeparator} />
-
-        <View style={styles.favebuttonContainer}>
+        {/* {favesButton} */}
+        {/* <View style={styles.favebuttonContainer}>
           <LinearGradient
             colors={["#cf392a", "#9963ea"]}
             start={{ x: 0.0, y: 1.0 }}
@@ -50,21 +127,11 @@ const Sessions = ({ session, speaker, navigation, favesFunctionality }) => {
               styles.favebutton
             ]}
           >
-            <TouchableOpacity
-              onPress={() => {
-                // console.log(favesFunctionality.getFavedSessionIds, "Functions");
-                const isFave = favesFunctionality.addFaves(session.id);
-                const notFave = favesFunctionality.removeFaves(session.id);
-                const checkFaves = favesFunctionality.getFavedSessionIds.includes(
-                  session.id
-                );
-              }}
-              // underlayColor="#fff"
-            >
+            <TouchableOpacity onPress={() => {}}>
               <Text style={styles.faveText}> Remove from Faves </Text>
             </TouchableOpacity>
           </LinearGradient>
-        </View>
+        </View> */}
       </View>
     </ScrollView>
   );
@@ -75,3 +142,15 @@ export default Sessions;
 
 // if there is faves - show the remove faves
 // if no faves - add faves
+
+{
+  (" ");
+}
+{
+  /* {checkFaves ? <Icon name="ios-heart" /> : null} */
+}
+{
+  /* {checkFaves ?  <Text style={styles.faveText}> Remove from Faves </Text> :  <Text style={styles.faveText}> Add to from Faves </Text> } */
+}
+
+//checkFaves ? notFave : isFave
