9ca77bee571c61a339e81aa3b6e222c0713e694a
Yen Yin Hing <yenyinf287@gmail.com>
2019-08-05 14:37:19 -0700

    finalize installation of react-native-gesture-handler for Android

--- android/app/src/main/java/com/r10/MainActivity.java
+++ android/app/src/main/java/com/r10/MainActivity.java
@@ -2,6 +2,12 @@ package com.r10;
 
 import com.facebook.react.ReactActivity;
 
+
+ import com.facebook.react.ReactActivityDelegate;
+ import com.facebook.react.ReactRootView;
+ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -12,4 +18,13 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "R10";
     }
+      @Override
+ protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+      return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
 }
